9862daa2b793abb5f98bb9c4672d701c10cd8867
Sergey Shvets <wert2all@gmail.com>
2019-07-29 20:14:37 +0300

    wert2all mf68 - done

--- keyboards/40percentclub/wert2all_mf68/config.h
+++ keyboards/40percentclub/wert2all_mf68/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { B6, B2, B3, B1, F7, F6, F5, F4 }
+#define MATRIX_ROW_PINS { F4, B3, F5, B2, F6, F7, B1, B6 }
 #define MATRIX_COL_PINS { D3, D2, D1, D0, D4, C6, D7, E6, B4 }
 #define UNUSED_PINS
 

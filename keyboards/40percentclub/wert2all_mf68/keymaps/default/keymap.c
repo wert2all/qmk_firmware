a2cbd9c0230e5250c0a5da8383cf267593488b66
Sergey Shvets <wert2all@gmail.com>
2019-09-20 02:27:35 +0300

    Added Win-lock

--- keyboards/40percentclub/wert2all_mf68/keymaps/default/keymap.c
+++ keyboards/40percentclub/wert2all_mf68/keymaps/default/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_VOLD, KC_END,
     _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,          _______,
     _______, _______, _______, _______, _______, _______, _______, KC_MUTE, _______, _______, _______,                   _______,          KC_MUTE,
-    _______, _______, _______,                            _______,                            _______, _______, _______,          KC_MPRV, KC_MPLY, KC_MNXT
+    _______, _______, _______,                            _______,                         MAGIC_NO_GUI, _______, MAGIC_UNNO_GUI, KC_MPRV, KC_MPLY, KC_MNXT
   ),
   [_FN2] = LAYOUT_68_ansi(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC,          KC_VOLU, KC_HOME,

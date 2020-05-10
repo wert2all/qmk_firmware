78fc4503dc86c4560e08b3adaa081a58371b4cef
wert2all <wert2all@gmail.com>
2020-05-10 19:33:12 +0300

    AutoShift support

--- keyboards/40percentclub/wert2all_mf68/rules.mk
+++ keyboards/40percentclub/wert2all_mf68/rules.mk
@@ -75,5 +75,5 @@ MIDI_ENABLE = no            # MIDI controls
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
-
+AUTO_SHIFT_ENABLE = yes 	# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_auto_shift.md
 LAYOUTS = 68_ansi

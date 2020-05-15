b7dca45c2656045c722ac28639548160f3a3ca84
wert2all <wert2all@gmail.com>
2020-05-15 18:58:43 +0300

    fix auto-shift modificator

--- keyboards/40percentclub/wert2all_mf68/keymaps/default/keymap.c
+++ keyboards/40percentclub/wert2all_mf68/keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC,          KC_VOLU, KC_HOME,
     _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_VOLD, KC_END,
     _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,          _______,
-    KC_ASTG, _______, _______, _______, _______, _______, _______, KC_MUTE, _______, _______, _______,                   _______,          KC_MUTE,
+    _______, _______, _______, _______, _______, _______, _______, KC_MUTE, _______, _______, _______,                   KC_ASTG,          KC_MUTE,
     _______, TO_QW,   _______,                            TO_DV,                         MAGIC_NO_GUI, _______, MAGIC_UNNO_GUI, KC_MPRV, KC_MPLY, KC_MNXT
   )
 };
